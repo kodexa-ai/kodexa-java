9ff9ccb0f164b0aafaacaa2c602407cd6aa49db7
Philip Dodds <philip@kodexa.com>
2020-11-12 06:30:24 -0500

    Support store ref in content object

--- src/main/java/com/kodexa/client/remote/ContentObject.java
+++ src/main/java/com/kodexa/client/remote/ContentObject.java
@@ -13,9 +13,20 @@ public class ContentObject {
 
     private String id;
     private String name;
+
     private List<String> labels = new ArrayList<>();
 
+    /**
+     * The content type (DOCUMENT/NATIVE)
+     */
     @JsonProperty("content_type")
     private String contentType;
 
+    /**
+     * The reference to the store holding this content object
+     * if it is null then the content object is in cache and part of a
+     * session
+     */
+    @JsonProperty("store_ref")
+    private String storeRef;
 }
