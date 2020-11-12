25338a51db3955234d8dba825bb34f0b0c71ae95
Philip Dodds <philip@kodexa.com>
2020-11-12 03:36:55 -0500

    Ignore unknown

--- src/main/java/com/kodexa/client/SourceMetadata.java
+++ src/main/java/com/kodexa/client/SourceMetadata.java
@@ -1,11 +1,13 @@
 package com.kodexa.client;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.Map;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SourceMetadata {
 
     @JsonProperty("original_filename")
