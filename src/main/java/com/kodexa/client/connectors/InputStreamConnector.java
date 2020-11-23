206a75e254a44aeff0e29d418ca791387475e56b
Philip Dodds <philip@kodexa.com>
2020-11-22 19:28:36 -0500

    Don't put the inputstream in the metadata

--- src/main/java/com/kodexa/client/connectors/InputStreamConnector.java
+++ src/main/java/com/kodexa/client/connectors/InputStreamConnector.java
@@ -4,12 +4,15 @@ import com.kodexa.client.Document;
 import lombok.NoArgsConstructor;
 
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 @NoArgsConstructor
 public class InputStreamConnector implements Connector {
 
     private InputStream inputStream;
     private boolean empty;
+    private Map<String, InputStream> cache = new HashMap<>();
 
     public InputStreamConnector(InputStream inputStream) {
         this.inputStream = inputStream;
@@ -23,9 +26,7 @@ public class InputStreamConnector implements Connector {
 
     @Override
     public InputStream getSource(Document document) {
-        InputStream inputStream = (InputStream) document.getMetadata().get("stream");
-        document.getMetadata().remove("stream");
-        return inputStream;
+        return cache.get(document.getUuid());
     }
 
     @Override
@@ -37,7 +38,7 @@ public class InputStreamConnector implements Connector {
     public Document next() {
         Document document = new Document();
         document.getMetadata().put("connector", getName());
-        document.getMetadata().put("stream", inputStream);
+        cache.put(document.getUuid(), inputStream);
         empty = true;
         return document;
     }
