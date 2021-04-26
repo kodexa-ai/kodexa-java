5ef560396287cf7e59e26293d4e34a69f3b3c755
Philip Dodds <philip@kodexa.com>
2021-04-26 12:05:12 -0400

    Ignore unknown stuff

--- src/main/java/com/kodexa/client/ContentClassification.java
+++ src/main/java/com/kodexa/client/ContentClassification.java
@@ -1,8 +1,10 @@
 package com.kodexa.client;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ContentClassification {
 
     private String label;
