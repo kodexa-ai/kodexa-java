c38fa39183705b0690b6002545c7c761e7a8b00a
Philip Dodds <philip@kodexa.ai>
2021-05-26 05:02:39 -0400

    Add the getAllContents method and also the uuid to the content node

--- src/main/java/com/kodexa/client/ContentNode.java
+++ src/main/java/com/kodexa/client/ContentNode.java
@@ -17,6 +17,8 @@ public class ContentNode {
 
     private int index;
 
+    private String uuid;
+
     @JsonProperty("node_type")
     private String type;
     private String content;
@@ -26,5 +28,20 @@ public class ContentNode {
 
     private List<ContentNode> children = new ArrayList<>();
     private List<ContentFeature> features = new ArrayList<>();
+
+    /**
+     * Returns all the contents from this node and all its children
+     *
+     * @param separator
+     * @return
+     */
+    public String getAllContents(String separator) {
+        List<String> allContents = new ArrayList<>();
+        allContents.add(this.content);
+        for (ContentNode child : this.children) {
+            allContents.add(child.getAllContents(separator));
+        }
+        return String.join(separator, allContents);
+    }
 
 }
