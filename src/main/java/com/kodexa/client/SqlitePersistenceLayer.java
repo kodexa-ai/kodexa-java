a2e3dbef1aa7d33ab5bcd21763b73bd0a471da16
Philip Dodds <philip@kodexa.ai>
2021-06-28 17:05:24 -0400

    Implement more of the unpack logic for KDDB format

--- src/main/java/com/kodexa/client/SqlitePersistenceLayer.java
+++ src/main/java/com/kodexa/client/SqlitePersistenceLayer.java
@@ -1,8 +1,12 @@
 package com.kodexa.client;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Jdbi;
+import org.msgpack.jackson.dataformat.MessagePackFactory;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -10,13 +14,27 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * A Persistence Layer that works with the document to allow it to be interacted with
  */
 public class SqlitePersistenceLayer {
+
+    private final static ObjectMapper OBJECT_MAPPER_MSGPACK;
+
+    Map<Integer, String> nodeTypes;
+
+    Map<Integer, String> featureTypeNames;
+
+    static {
+        OBJECT_MAPPER_MSGPACK = new ObjectMapper(new MessagePackFactory());
+        OBJECT_MAPPER_MSGPACK.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
 
     private String dbPath;
     private Jdbi jdbi;
@@ -60,6 +78,35 @@ public class SqlitePersistenceLayer {
         // This method will update the document to match the contents
         // of the KDDB database
         jdbi.withHandle(handle -> {
+
+            // We get all the metadata back
+            try {
+                byte[] metadataPack = (byte[]) handle.createQuery("SELECT * FROM metadata")
+                        .mapToMap()
+                        .first().get("metadata");
+
+                Document baseDocument = OBJECT_MAPPER_MSGPACK.readValue(metadataPack, Document.class);
+                document.setSource(baseDocument.getSource());
+                document.setClasses(baseDocument.getClasses());
+                document.setLabels(baseDocument.getLabels());
+                document.setMetadata(baseDocument.getMetadata());
+                document.setUuid(baseDocument.getUuid());
+
+
+                // Lets get all the node types and feature type/name combinations
+                nodeTypes =
+                        handle.createQuery("SELECT id, name FROM n_type")
+                                .mapToMap()
+                                .collect(Collectors.toMap(x -> Integer.valueOf(String.valueOf(x.get("id"))), x -> String.valueOf(x.get("name"))));
+
+                featureTypeNames =
+                        handle.createQuery("SELECT id, name FROM f_type")
+                                .mapToMap()
+                                .collect(Collectors.toMap(x -> Integer.valueOf(String.valueOf(x.get("id"))), x -> String.valueOf(x.get("name"))));
+            } catch (IOException e) {
+                throw new KodexaException("Unable to unpack metadata", e);
+            }
+
             List<Map<String, Object>> contentNodes =
                     handle.createQuery("SELECT id, nt, pid FROM cn where pid is null")
                             .mapToMap()
@@ -76,6 +123,30 @@ public class SqlitePersistenceLayer {
     private ContentNode buildNode(Map<String, Object> contentNodeValues, Handle handle) {
         ContentNode contentNode = new ContentNode();
         contentNode.setUuid(String.valueOf(contentNodeValues.get("id")));
+
+        List<Map<String, Object>> features =
+                handle.createQuery("SELECT id, f_type, fvalue_id FROM f where cn_id is :nodeId").bind("nodeId", contentNode.getUuid())
+                        .mapToMap()
+                        .list();
+
+        for (Map<String, Object> feature : features) {
+            Map<String, Object> featureValue =
+                    handle.createQuery("SELECT binary_value, single FROM f_value where id is :fvalue_id").bind("fvalue_id", feature.get("fvalue_id"))
+                            .mapToMap()
+                            .first();
+            ContentFeature contentFeature = new ContentFeature();
+            contentFeature.setFeatureType(featureTypeNames.get(feature.get("f_type")).split(":")[0]);
+            contentFeature.setName(featureTypeNames.get(feature.get("f_type")).split(":")[1]);
+            contentFeature.setSingle(Integer.valueOf(1) == featureValue.get("single"));
+            TypeReference<ArrayList<Object>> typeRef
+                    = new TypeReference<>() {
+            };
+            try {
+                contentFeature.setValue(OBJECT_MAPPER_MSGPACK.readValue((byte[]) featureValue.get("binary_value"), typeRef));
+            } catch (IOException e) {
+                throw new KodexaException("Unable to unpack value for feature", e);
+            }
+        }
 
         List<Map<String, Object>> childNodes =
                 handle.createQuery("SELECT id, nt, pid FROM cn where pid is :nodeId").bind("nodeId", contentNode.getUuid())
