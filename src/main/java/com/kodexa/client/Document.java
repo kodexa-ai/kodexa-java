dcc9c40c741047b1740d5a53a9dcc80c3b634053
Philip Dodds <philip@kodexa.ai>
2021-06-28 03:33:13 -0400

    Ignore the serialization layer

--- src/main/java/com/kodexa/client/Document.java
+++ src/main/java/com/kodexa/client/Document.java
@@ -1,5 +1,6 @@
 package com.kodexa.client;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -48,6 +49,7 @@ public class Document {
         persistenceLayer.loadDocument(this);
     }
 
+    @JsonIgnore
     private SqlitePersistenceLayer persistenceLayer;
 
     private Map<String, Object> metadata = new HashMap<>();
