936b803019f0d0f230752460c41a613416549fa4
Philip Dodds <philip.dodds@me.com>
2023-09-19 11:26:25 -0400

    We need to make sure that SourceMetadata can be serialized

--- src/main/java/com/kodexa/client/SourceMetadata.java
+++ src/main/java/com/kodexa/client/SourceMetadata.java
@@ -4,11 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Map;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class SourceMetadata {
+public class SourceMetadata implements Serializable {
 
     @JsonProperty("original_filename")
     private String originalFilename;
